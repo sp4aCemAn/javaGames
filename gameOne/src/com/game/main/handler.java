e220a62758add6512741dbd57363cb06518d4542
sp4aCemAn <jamesshufeltiii@gmail.com>
2023-08-29 11:38:29 -0500

    comment on what to work on during luch prob

--- gameOne/src/com/game/main/handler.java
+++ gameOne/src/com/game/main/handler.java
@@ -18,6 +18,15 @@ public class handler {
         for (gameObject gameObj : objects)
         {
         //tick all objects in list
+        /*TODO implement hitbox
+         * essentually we have to have to compare whether or not each object is
+         * within  a rage of eachother so if we call data from plater object (x to x + sizeOfplayer) this goes for y axis is intersecting
+         * with the rage and domain of some other object (this.x to this.x + this.SizeOfObject) actual attribute names are obviously diffrent
+         * as they extend from the game object class and have predifind attributes and accesor and modifire classes however 
+         * this is just a representation of how to handle hitbox detection its sorta primitive and probebly not efficient
+         * as we are calculating this every tick but idk im not a game dev
+         * 
+         */
             gameObject tempObject = gameObj;
             tempObject.tick();
     
