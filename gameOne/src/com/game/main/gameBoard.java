b50dd85b2b552cf999e5162db65d9a0c1d5f4d58
sp4aCemAn <jamesshufeltiii@gmail.com>
2023-09-05 17:55:19 -0500

    refactored and added documentation for gameBoard

--- gameOne/src/com/game/main/gameBoard.java
+++ gameOne/src/com/game/main/gameBoard.java
@@ -4,40 +4,60 @@ import java.awt.Graphics;
 
 // TODO REFATOR!!!!! 
 public class gameBoard
-{
+{   
+    
     protected int HIGHT,WIDTH;
     private int UNIT_SIZE; 
     private int UNIT_PER_ROW; // tiles per row
     public Color COLOR_ONE, COLOR_TWO;
     public Tile objectGrid[][];
 
-    //create array of game objects to ref X and Y pos from
-    public gameBoard(int Width, int Hight, int Tpr){
-    HIGHT = Hight;
-    WIDTH = Width;
-    UNIT_PER_ROW = Tpr;
-    COLOR_ONE = Color.WHITE;
-    COLOR_TWO = Color.BLACK;
-    UNIT_SIZE = (Hight) / UNIT_PER_ROW;
-    objectGrid  = new Tile[UNIT_PER_ROW][UNIT_PER_ROW];
-    
-    
-        int Ypoint = 0;
+    /*
+     * this class creates a 2D array of objects (tiles)
+     * to refrence x and y pos for grid based movement 
+     * sorta like a ches board if press up refence to the tile above the player in the 2d grid array 
+     * visual representation     
+     * 
+     * {[#,#,#,#,#,#,#],
+     * [#,#,#,#,#,#,#],
+     * [#,#,#,p,#,#,#], So if the player which is represented as p wants to go up refrence to the 
+     * [#,#,#,#,#,#,#], the Tile object above the player == > if player presses up p[3][3] >> p[3][2] update player location to location of that Tile obj 
+     * [#,#,#,#,#,#,#],}of course the player isint stored in the array however the tile in which this player resides on is detected by the hitbox 
+     *                  the players locateion is updated to the location of the Tile in question 
+     *                  location of the player in refrence to the game board is not stored however hitbox detection is what deciedes the interacion of 
+     *                  and location of in essence (plaer is coliding with tile on [3][3]) this is probebly going to be changed as 
+     *                  hit box calc is more computationally expensive than just storeing the player and obj location and so this might change
+     *                  rn its just if (player) collides with (apple) do thing this might change later on how the spesific code for hitbox det works
+     */
 
-        
+    //CONSTRUCTOR 
+    public gameBoard(int Width, int Hight, int Tpr)
+    {
+
+        HIGHT = Hight;
+        WIDTH = Width;
+        UNIT_PER_ROW = Tpr;
+        COLOR_ONE = Color.WHITE;
+        COLOR_TWO = Color.BLACK;
+        UNIT_SIZE = (Hight) / UNIT_PER_ROW;
+        objectGrid  = new Tile[UNIT_PER_ROW][UNIT_PER_ROW]; 
+    
+
+    // populate array
+        int Ypoint = 0; // keeps track of y pos of grid
         
         for (int i = 0; i < objectGrid.length; i++) 
         {
-            int Xpoint = 0;
+            int Xpoint = 0; // keeps track of x pos of grid 
             for (int j = 0; j < objectGrid[i].length; j++)
             {
                 objectGrid[i][j] = new Tile(Xpoint, Ypoint, UNIT_SIZE, ID.Tile);
                 Xpoint += UNIT_SIZE;
                 
             }
-        Ypoint += UNIT_SIZE;
+            Ypoint += UNIT_SIZE;
 
-    }
+        }
 }
 
 
@@ -46,20 +66,22 @@ public class gameBoard
     
     
 
- 
+    // render Grid 
     public void render(Graphics g) // render grid
     {
-    
     boolean BorW = false;
-        //System.out.println("first object of array " + objectGrid[0][0] + " " + objectGrid[0][0].getX() + " " + objectGrid[0][0].getY());
-    for (Tile[] gameObjects : objectGrid) {
-        for (Tile gameObject : gameObjects) {
+    //System.out.println("first object of array " + objectGrid[0][0] + " " + objectGrid[0][0].getX() + " " + objectGrid[0][0].getY()); DEBUG
+        for (Tile[] gameObjects : objectGrid) // loops through entire array and calls render function of each tile obj
+        {
+            for (Tile gameObject : gameObjects) 
+            {
 
             //System.out.println(gameObject + " " + gameObject.getX() + " " + gameObject.getY());
-                if(BorW == true)
+                
+                // this alternates between color 1 and 2 and renders each color accordingly
+                if(BorW == true) 
                 {
                     gameObject.color = COLOR_ONE;
-                
                 }
                 else
                 {
@@ -69,11 +91,7 @@ public class gameBoard
 
                 gameObject.render(g);
 
-            }
-            
-
-            
-            
+            }   
         }
     }
     
