21bd53dae9337b57bd54eb1827cb0f716f26237c
sp4aCemAn <jamesshufeltiii@gmail.com>
2023-09-19 00:41:11 -0500

    1st step of movement overhall

--- gameOne/src/com/game/main/gridMovement.java
+++ gameOne/src/com/game/main/gridMovement.java
@@ -22,31 +22,29 @@ public class gridMovement
 
     public void animate(gameObject player,int newPosX,int newPosY)
     {
-        
-        int target = 20;
-        int test = 0;
-        while(player.x != newPosX || player.y != newPosY)
-        {
+                 
             if(player.x < newPosX)
             {
-                player.velX =1;
+                player.setX(player.getX()+1);
+                
             }
             if(player.x > newPosX)
             {
-                player.velX = -1;
+                player.setX(player.getX()-1);
+                
             }
             if(player.y < newPosY)
             {
-                player.velY =1;
+                player.setY(player.getY()+1);
+                
             }
             if(player.y > newPosY)
             {
-                player.velY = -1;
+                player.setY(player.getY()-1);
+                
+            }else{
+               
             }
-            
-        }
-        player.velX = 0;
-        player.velY = 0;
         
         
         
@@ -61,11 +59,18 @@ public class gridMovement
     // update player movement to tile dep on vec (arr) 
     public void movePlayer(gameObject player,int direction[])
     {
+        
+
         player.sizeX = targetGrid[0][0].sizeX; // update player size will be removed later 
         player.sizeY = targetGrid[0][0].sizeY;
+
+        
+       
         gridPosX += direction[0];  // store "vector" in X and Y values to ref for velX velY
         gridPosY += direction[1]; // if only vectors existed in the way i want them to work would make code more readable 
         direction = direction;
+
+
         if(gridPosX < 0)
         {
             gridPosX = 0;
@@ -82,12 +87,17 @@ public class gridMovement
         {
             gridPosY = targetGrid[0].length-1;
         }
-
+        
         targetPos[0] = targetGrid[gridPosY][gridPosX].x;
         targetPos[1] = targetGrid[gridPosY][gridPosX].y;
         
-
         animate(player, targetPos[0], targetPos[1]);
+       
+
+
+        
+        
+        
         
         System.out.println(gridPosX +  " " + gridPosY);
         
